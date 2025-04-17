045c2629d3a341ac0f52cd6d238f9abbd8cb1b3b
DCodeProg <danael.legrand@outlook.fr>
2025-04-17 10:25:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_epsilon_v2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_epsilon_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_epsilon_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
