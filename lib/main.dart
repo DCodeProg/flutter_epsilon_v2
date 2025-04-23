527f04840a27d405d9730ea21493a3a5c38297e3
DCodeProg <danael.legrand@outlook.fr>
2025-04-23 12:22:26 +0200

    fix: add missing app_router import

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
+import 'core/router/app_router.dart';
 import 'core/shared/cubit/theme_cubit.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/text_theme.dart';
