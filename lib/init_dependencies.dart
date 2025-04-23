b7dfc766162340eb78bfea2d8610aa77a74e6eaf
DCodeProg <danael.legrand@outlook.fr>
2025-04-23 12:22:06 +0200

    feat: supabase linking

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'core/secrets/app_secrets.dart';
 import 'core/shared/cubit/theme_cubit.dart';
 
 GetIt getIt = GetIt.instance;
 
 Future<void> initDependencies() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
+  final supabase = await Supabase.initialize(
+    url: AppSecrets.supabaseUrl,
+    anonKey: AppSecrets.supabaseAnnonKey,
+  );
+
+  getIt.registerLazySingleton(() => supabase.client);
+
   HydratedBloc.storage = await HydratedStorage.build(
     storageDirectory:
         kIsWeb
